96450c69bbbf82407eab78ce71ade4d5ea43ae6d
zhendolion <zhendolion2014@gmail.com>
2024-12-07 01:30:00 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from budget_app import app
+import os
+from application import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
